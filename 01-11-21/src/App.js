7f3bec92f3dd32ae0c3592bba6045e01a24b3fa4
Honza-H <jan.hlavacek25@gmail.com>
2021-11-22 16:46:44 +0100

    22.11 - tailwind

--- 01-11-21/src/App.js
+++ 01-11-21/src/App.js
@@ -4,29 +4,40 @@ import { useState } from "react";
 import { useEffect } from "react";
 import ContentItem from './contentItem';
 
+
+
 function App() {
-  const [count, setCount] = useState(0);
-  const handleClick = () => {
-    console.log("User click");
-    setCount(count + 1);
+
   }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Text</p>
-        <p>Hodnota počtu: {count}</p>
-        <div>
-          <button onClick={handleClick}>Přičti</button>
-        </div>
-        <div>
-          <ContentItem/>
-          <ContentItem text="aa"/>
-        </div>
     
-      </header>
-    </div>
   );
 }
+
+// function App() {
+//   const [count, setCount] = useState(0);
+//   const handleClick = () => {
+//     console.log("User click");
+//     setCount(count + 1);
+//   }
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <p>Text</p>
+//         <p>Hodnota počtu: {count}</p>
+//         <div>
+//           <button onClick={handleClick}>Přičti</button>
+//         </div>
+//         <div>
+//           <ContentItem/>
+//           <ContentItem text="aa"/>
+//         </div>
+  
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
